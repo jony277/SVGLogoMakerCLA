fe7fca6c8f3830cda40ddaf7598532075694cc8d
[Jonthan Gutierrez] <jonigutierrez741@gmail.com>
2023-09-24 00:56:21 -0400

    refactored the code in order to change and delete some files from lib

--- index.js
+++ index.js
@@ -1,21 +1,69 @@
-const { collectUserInput } = require('./lib/userInput');
-const { generateSVG } = require('./lib/generateSVG');
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const {Triangle, Square} = require('./lib/shapes')
 
 
-async function startApp() {
-  try {
-   
-    const userInput = await collectUserInput();
+function promptUser(){
+  inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'What shape do you want?',
+      choices:['Triangle', 'Circle', 'Square'],
+    },
+    {
+      type: 'list',
+      message: 'What is your preferred color',
+      name: 'color',
+      choices: ['Red', 'Yellow', 'Blue'],
+    },
+    {
+      type: 'input',
+      message: 'What Text do you want to show (3 characters max)?',
+      name: 'text',
+    },
+  ])
+  .then((answer) => {
+    if (answer.text.length >3){
+      console.log("3 Characters max");
+      promptUser();
+    }else{
+      console.log(answer);
+      writeFile('logo.svg',answer);
+    }
+  });
 
-    
-    await generateSVG(userInput);
-
-    console.log('Generated logo.svg');
-  } catch (error) {
-    console.error('An error occurred:', error);
-    process.exit(1);
-  }
 }
 
+function writeFile(fileName, answer){
+  let svg ="";
+  svg = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+  svg += '<g>';
+  svg += `${answer.shape}`;
 
-startApp();
+  let shapeChoice;
+  switch(answer.shape){
+    case "Triangle": 
+      shapeChoice = new Triangle();
+      svg += `<polygon points="150, 18 244, 182 56, 182" fill="${answer.color}"/>`;
+      break;
+    case "Square":
+      shapeChoice = new Square();
+      svg += `<rect x="73" y="40" width="160" height="160" fill="${answer.color}"/>`;
+      break;
+    default:
+      console.log("Choose only Square or Triangle");
+      promptUser();
+  }
+  svg += `<text x="150" y="130" text-anchor="middle" font-size="40">${answer.text}</text>`;
+svg += "</g>";
+svg += "</svg>";
+
+fs.writeFile(fileName, svg, (err) => {
+  err ? console.log(err) : console.log("Generated logo.svg");
+});
+}
+
+promptUser();
